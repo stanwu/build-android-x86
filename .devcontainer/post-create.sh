ae1b62bf6dd28de8f935200f8a5dbc2ecb0cf12f
Stan Ht. Wu <stanwu.tw@gmail.com>
2024-06-09 14:00:30 +0000

    Add GCP token support

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,3 +9,6 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get update
 sudo apt-get -y install google-cloud-sdk
+
+echo "$GCLOUD_AUTH" > key-file.json
+gcloud auth activate-service-account --key-file=key-file.json
